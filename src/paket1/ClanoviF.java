79c8d5957e4e291b8b4838548d2a72c023e004a4
MihajloJankovic <101173449+MihajloJankovic@users.noreply.github.com>
2022-06-09 15:47:40 +0200

    git UPDATE 6
    f

--- src/paket1/ClanoviF.java
+++ src/paket1/ClanoviF.java
@@ -6,12 +6,18 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ClanoviF extends JFrame {
 
@@ -51,13 +57,25 @@ public class ClanoviF extends JFrame {
 	/**
 	 * Initialize the contents of the frame.
 	 */
+	
+	public static boolean isNumeric(String strNum) {
+	    if (strNum == null) {
+	        return false;
+	    }
+	    try {
+	        Integer d = Integer.valueOf(strNum);
+	    } catch (NumberFormatException nfe) {
+	        return false;
+	    }
+	    return true;
+	}
 	private void initialize() {
 		frame = new JFrame();
 		frame.setVisible(true);
 		frame.getContentPane().addComponentListener(new ComponentAdapter() {
 
 		});
-		frame.setBounds(100, 100, 675, 562);
+		frame.setBounds(100, 100, 675, 616);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Name :");
@@ -104,7 +122,7 @@ public class ClanoviF extends JFrame {
 		comboBox.setBounds(251, 192, 211, 31);
 		frame.getContentPane().add(comboBox);
 		
-		textField.setText(jo.Adresa);
+		textField.setText(jo.Ime);
 		textField_1.setText(jo.Prezime);
 		textField_2.setText(jo.Adresa);
 		if(jo.Pol == true)
@@ -131,7 +149,7 @@ public class ClanoviF extends JFrame {
 		comboBox_1.setBounds(251, 247, 211, 31);
 		frame.getContentPane().add(comboBox_1);
 		
-		String[] ag = {"True", "False"};
+		String[] ag = {"true", "false"};
 		JComboBox comboBox_2 = new JComboBox(ag);
 		comboBox_2.setBounds(251, 298, 211, 31);
 		frame.getContentPane().add(comboBox_2);
@@ -141,6 +159,7 @@ public class ClanoviF extends JFrame {
 		frame.getContentPane().add(lblNewLabel_6);
 		
 		textField_3 = new JTextField();
+		textField_3.setText("0");
 		textField_3.setBounds(251, 349, 211, 31);
 		frame.getContentPane().add(textField_3);
 		textField_3.setColumns(10);
@@ -149,12 +168,96 @@ public class ClanoviF extends JFrame {
 		comboBox_2.setSelectedItem(String.valueOf(jo.Aktivan));
 		
 		JLabel lblNewLabel_7 = new JLabel("Number of paid months :");
-		lblNewLabel_7.setBounds(86, 410, 135, 31);
+		lblNewLabel_7.setBounds(86, 404, 135, 31);
 		frame.getContentPane().add(lblNewLabel_7);
 		
 		textField_4 = new JTextField();
+		textField_4.setText("0");
 		textField_4.setBounds(251, 404, 211, 31);
 		frame.getContentPane().add(textField_4);
 		textField_4.setColumns(10);
+		textField_3.setText(String.valueOf(jo.BrojCK));
+		textField_4.setText(String.valueOf(jo.BrojUplacenihMeseci));
+		
+		JComboBox comboBox_3 = new JComboBox(ag);
+		comboBox_3.setBounds(251, 458, 211, 31);
+		frame.getContentPane().add(comboBox_3);
+		
+		JLabel lblNewLabel_8 = new JLabel("deleted :");
+		lblNewLabel_8.setBounds(86, 458, 123, 31);
+		frame.getContentPane().add(lblNewLabel_8);
+		comboBox_3.setSelectedItem(String.valueOf(jo.Obrisan));
+		
+		JButton btnNewButton = new JButton("Save");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String ime1 = (textField.getText());
+				String Prezime = (textField_1.getText());
+				String Adresa = (textField_2.getText());
+				
+				boolean pol = Boolean.valueOf(String.valueOf(comboBox.getSelectedItem()));
+				String tipc = String.valueOf(comboBox_1.getSelectedItem());
+				boolean aktivan = Boolean.valueOf(String.valueOf(comboBox_2.getSelectedItem()));
+				boolean obrisan = Boolean.valueOf(String.valueOf(comboBox_3.getSelectedItem()));
+				String bm = String.valueOf(textField_4.getText());
+						
+				if(ime1.length()>3)
+				{
+					if(Prezime.length()>3)
+					{
+						if(Adresa.length()>3)
+						{
+							if(isNumeric(bm)==true)
+							{
+								jo.Ime = ime1;
+								jo.Prezime = Prezime;
+								jo.Adresa = Adresa;
+								jo.Pol = pol;
+								jo.Aktivan = aktivan;
+								jo.Obrisan = obrisan;
+								int  d = Integer.valueOf(bm);
+								jo.BrojUplacenihMeseci = d;
+								for(int i=0;i<metode.tc.size();i++)
+								{	
+									TipClanarine temp = metode.tc.get(i);
+									if(tipc == temp.ime)
+									{
+										jo.TipClanarine = temp;
+									}
+								}
+								try {
+									metode.upisiSVE();
+								} catch (IOException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+							}
+							else
+							{
+								JOptionPane.showMessageDialog(null, "Number of paid monts is no Ineger");
+							}
+							
+						}
+						else
+						{
+							JOptionPane.showMessageDialog(null, "Adress too short");
+						}
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null, "Surname too short");
+					}
+					
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Name too short");
+				}
+		
+			
+			}
+		});
+		btnNewButton.setBounds(237, 521, 123, 45);
+		frame.getContentPane().add(btnNewButton);
 	}
 }
